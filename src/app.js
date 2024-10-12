512eb8bd5a7b1653696adaa322862f04e5002c88
Shivam Barot <shivambarot0806@gmail.com>
2024-10-12 19:00:31 +0530

    all setup is ready

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// router import(this is a standardized method of importing router)
+import userRouter from "./routes/user.routes.js";
+
+// we need middleware to use router that's why .use() is used instead of get and post and listen
+app.use("/api/v1/users", userRouter);
+
 export { app }
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -13,9 +13,9 @@ class ApiError extends Error {
         this.errors = errors
         
         if (stack) {
-            this.stack = stack
+            this.stack = stack;
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
