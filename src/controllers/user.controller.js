d6850c82908c012592b7fc95ccbbd989d82584bf
Shivam Barot <shivambarot0806@gmail.com>
2024-10-26 19:55:26 +0530

    channel profile using aggregate pipeline added in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -331,6 +331,76 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    
+    const {username} = req.params;
+    if (!username) {
+        throw new ApiError(400, "Username required");
+    }
+    
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                subscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                subscribedToCount: 1,
+                subscribersCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1
+            }
+        }
+    ]);
+
+    console.log(channel);
+
+    if (!channel.length) {
+        throw new ApiError(404, "channel does not exists");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched successfully"));
+});
+
 export { 
     registerUser, 
     loginUser, 
